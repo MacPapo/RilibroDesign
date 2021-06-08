3728fa45df70af1341d010497913ea6b5b9b0ccf
Matteo Zambon <matteozambon@MBP-di-Matteo.lan>
2021-06-08 11:38:14 +0200

    /sell added

--- client/src/pages/sell/sell.component.jsx
+++ client/src/pages/sell/sell.component.jsx
@@ -1,10 +1,167 @@
-import React from "react";
+import React, { useState } from "react";
+
 import Footer from "../../components/footer.component";
 import Nav from "../../components/navbar.component";
 
 export const Sell = () => {
+  const [count, setCount] = useState(1);
+
   return (
-    <div>Ciao</div>
+    <>
+      <div className="h-screen">
+        <div className="justify-center h-80 bg-register items-top">
+          <Nav />
+          <div className="pt-16 justify-items-center ">
+            <div className="flex items-center justify-center flex-1 ">
+              <div className="w-full px-4 py-10 text-center transition duration-700 border-gray-300 rounded-xl sm:border-2 lg:px-24 lg:max-w-xl sm:max-w-md bg-gray-50 bg-opacity-70 hover:bg-opacity-100 border-opacity-90">
+                <h1 className="w-full mb-4 text-3xl font-bold tracking-wider text-gray-600 hover:text-green-700">
+                  Pubblica Annuncio
+                </h1>
+                <div className="flex justify-center my-6">
+                  <div className="inline-flex h-1 bg-green-400 rounded-full w-36 "></div>
+                </div>
+                <h1 className="w-full mb-4 text-xl font-bold tracking-wider text-gray-600 hover:text-green-700">
+                  Step {count} di 3
+                </h1>
+                <form className="p-1 text-center">
+                  {count === 1 ? (
+                    <>
+                      <div className="py-2 text-left">
+                        <p className="px-2 font-semibold text-left text-s">
+                          Titolo
+                        </p>
+                        <input
+                          type="text"
+                          className="block w-full px-4 py-2 bg-gray-200 shadow-inner rounded-xl focus:outline-none focus:border-green-700"
+                          placeholder="Titolo"
+                          name="titolo"
+                          required
+                        />
+                      </div>
+                      <div className="py-2 text-left">
+                        <p className="px-2 font-semibold text-left text-s">
+                          Sottotitolo
+                        </p>
+                        <input
+                          type="text"
+                          className="block w-full px-4 py-2 bg-gray-200 shadow-inner rounded-xl focus:outline-none focus:border-green-700"
+                          placeholder="Sottotitolo"
+                          name="sottotitolo"
+                          required
+                        />
+                      </div>
+                      <div className="py-2 text-left">
+                        <p className="px-2 font-semibold text-left text-s">
+                          Autore
+                        </p>
+                        <input
+                          type="text"
+                          className="block w-full px-4 py-2 bg-gray-200 shadow-inner rounded-xl focus:outline-none focus:border-green-700"
+                          placeholder="Autore"
+                          name="autore"
+                          required
+                        />
+                      </div>
+                    </>
+                  ) : null}
+                  {count === 2 ? (
+                    <>
+                      <div className="py-2 text-left">
+                        <p className="px-2 font-semibold text-left text-s">
+                          ISBN
+                        </p>
+                        <input
+                          type="text"
+                          className="block w-full px-4 py-2 bg-gray-200 shadow-inner rounded-xl focus:outline-none focus:border-green-700"
+                          placeholder="ISBN"
+                          name="isbn"
+                          maxLength="10"
+                          required
+                        />
+                      </div>
+
+                      <div className="flex flex-col text-sm">
+                        <p className="px-2 font-semibold text-left text-s">
+                          Descrizione
+                        </p>
+                        <textarea
+                          className="block w-full px-4 py-2 bg-gray-200 shadow-inner rounded-xl focus:outline-none focus:border-green-700"
+                          placeholder="Descrizione del libro"
+                        ></textarea>
+                      </div>
+
+                      <div className="items-center mt-4 text-sm text-gray-700 options md:flex md:space-x-6">
+                        <p className="px-2 font-semibold text-left text-s">
+                          Condizione{" "}
+                        </p>
+                        <select className="w-full p-2 border border-gray-200 focus:outline-none focus:border-gray-500">
+                          <option value="select" selected>
+                            Imposta Stato
+                          </option>
+                          <option value="ottimo">Ottimo</option>
+                          <option value="buono">Buono</option>
+                          <option value="cattive">Cattivo</option>
+                        </select>
+                      </div>
+                    </>
+                  ) : null}
+                  {count === 3 ? (
+                    <>
+                      <div className="h-64 max-w-lg mx-6 my-40 shadow-lg md:flex md:mx-auto md:max-w-2xl">
+                        <img
+                          className="object-cover w-full h-full rounded-lg rounded-r-none md:w-1/3 pb-5/6"
+                          src="https://ik.imagekit.io/q5edmtudmz/FB_IMG_15658659197157667_wOd8n5yFyXI.jpg"
+                          alt="bag"
+                        />
+                        <div className="w-full px-4 py-4 bg-white rounded-lg md:w-2/3">
+                          <div className="flex items-center">
+                            <h2 className="mr-auto text-xl font-medium text-gray-800">
+                              Your Travel Buddy
+                            </h2>
+                            <p className="font-semibold tracking-tighter text-gray-800">
+                              only
+                              <i className="text-gray-600 line-through">60$</i>
+                              48$
+                            </p>
+                          </div>
+                          <p className="mt-4 text-sm text-gray-700">
+                            Lorem, ipsum dolor sit amet consectetur Amet
+                            veritatis ipsam reiciendis numquam tempore commodi
+                            ipsa suscipit laboriosam, sit earum at sequ
+                            adipisicing elit. Amet veritatis ipsam reiciendis
+                            numquam tempore commodi ipsa suscipit laboriosam,
+                            sit earum at sequi.
+                          </p>
+                        </div>
+                      </div>
+                    </>
+                  ) : null}
+                </form>
+                <div className="grid grid-cols-2 gap-2">
+                  <button
+                    className="block w-full p-2 font-bold tracking-wider text-white bg-red-300 rounded-xl hover:bg-red-400"
+                    type="submit"
+                    onClick={() => setCount(count - 1)}
+                    disabled={count === 1}
+                  >
+                    Indietro
+                  </button>
+                  <button
+                    className="block w-full p-2 font-bold tracking-wider text-white bg-green-300 rounded-xl hover:bg-green-700"
+                    type="submit"
+                    onClick={() => setCount(count + 1)}
+                    disabled={count > 2}
+                  >
+                    Avanti
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <Footer />
+        </div>
+      </div>
+    </>
   );
 };
 
