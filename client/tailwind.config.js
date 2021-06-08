bed08d68297969cac4908117e9c1d480af905e17
Jacopo Costantini <jacopocostantini32@gmail.com>
2021-06-08 10:58:16 +0200

    Homepage BG Fixed

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: "media", // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        "homepage-fixed": "url('assets/images/WP01.jpg')",
+        "homepage-phone": "url('assets/images/WP00.jpg')",
+      }),
+    },
   },
   variants: {
     extend: {},
