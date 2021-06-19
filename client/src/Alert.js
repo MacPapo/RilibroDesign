c508b938c2c02d81b1559a2bf634717fb4b9aab5
Matteo Zambon <zambonm8@gmail.com>
2021-06-19 08:59:23 +0200

    comune fixxed

--- client/src/Alert.js
+++ client/src/Alert.js
@@ -5,7 +5,7 @@ import { connect } from 'react-redux';
 const Alert = ({ alerts }) =>
 
   alerts.map((alert) => (
-    <div className="relative py-4 text-white bg-red-500 border-0">
+    <div className="static py-4 text-white bg-red-500 border-0 ">
       <div key={alert.id} className={`alert alert-${alert.alertType}`}>
         {alert.msg}
       </div>
--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -12,67 +12,70 @@ import {
 } from "./types";
 
 export const addPost = (formData) => async (dispatch) => {
-  var requestOptions = {
-    method: "GET",
-    redirect: "follow",
-  };
-
-  let myResponse;
-
-  const response = await fetch(
-    `https://api.rilibro.it/v1/towns/getTownInfo?nome=${formData.comune}`,
-    requestOptions,
-    {}
-  )
-    .then((response) => response.json())
-    .then((result) => (myResponse = result))
-    .catch((error) => console.log("error", error));
-
-  console.log("Regione e Provincia", myResponse);
-
-  console.log(
-    "Sono dentro la funzione add Post, i dati passati sono --> ",
-    formData,
-    "il token vale --> ",
-    localStorage.getItem("token")
-  );
-
-  const body = {
-    libro: {
-      titolo: formData.titolo,
-      prezzo: formData.prezzo,
-      autore: formData.autore,
-      ISBN: formData.isbn,
-      condizione: formData.condizione,
-      immagine:
-        "https://res.cloudinary.com/rilibro/image/upload/v1620286954/default-rilibro_p1prll.jpg",
-      descrizione: formData.descrizione,
-    },
-    utente: formData.id,
-    location: {
-      regione: myResponse.nome_regione,
-      provincia: myResponse.nome_provincia,
-      comune: myResponse.nome_comune,
-    },
-  };    
-
   try {
-    const res = await api.post("/v1/posts/add", JSON.stringify(body), {
-      headers: { "x-auth-token": localStorage.getItem("token") },
-    });
+    var requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
 
-    console.log("Risposta delle variabile res --> ", res);
+    let myResponse;
 
-    dispatch({
-      type: ADD_POST,
-      payload: res.data,
-    });
+    const response = await fetch(
+      `https://api.rilibro.it/v1/towns/getTownInfo?nome=${formData.comune}`,
+      requestOptions,
 
-    dispatch(setAlert("Post Created", "success"));
-  } catch (err) {
-    dispatch({
-      type: POST_ERROR,
-      payload: { msg: err.response.statusText, status: err.response.status },
-    });
+    )
+      .then((response) => response.json())
+      .then((result) => (myResponse = result))
+
+    console.log("Regione e Provincia", myResponse);
+
+    console.log(
+      "Sono dentro la funzione add Post, i dati passati sono --> ",
+      formData,
+      "il token vale --> ",
+      localStorage.getItem("token")
+    );
+
+    const body = {
+      libro: {
+        titolo: formData.titolo,
+        prezzo: formData.prezzo,
+        autore: formData.autore,
+        ISBN: formData.isbn,
+        condizione: formData.condizione,
+        immagine:
+          "https://res.cloudinary.com/rilibro/image/upload/v1620286954/default-rilibro_p1prll.jpg",
+        descrizione: formData.descrizione,
+      },
+      utente: formData.id,
+      location: {
+        regione: myResponse.nome_regione,
+        provincia: myResponse.nome_provincia,
+        comune: myResponse.nome_comune,
+      },
+    };
+
+    try {
+      const res = await api.post("/v1/posts/add", JSON.stringify(body), {
+        headers: { "x-auth-token": localStorage.getItem("token") },
+      });
+
+      console.log("Risposta delle variabile res --> ", res);
+
+      dispatch({
+        type: ADD_POST,
+        payload: res.data,
+      });
+
+      dispatch(setAlert("Post Created", "success"));
+    } catch (err) {
+      dispatch({
+        type: POST_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status },
+      });
+    }
+  } catch (error) {
+    dispatch(setAlert("Il comune non esiste", "danger"));
   }
 };
